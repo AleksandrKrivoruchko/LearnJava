a0aeed774a858ccfb61ef0fae36a50ab6a130f8c
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-07-03 21:26:30 +0300

    Added new method

--- seminar4/seminar4/homework4/homewok.java
+++ seminar4/seminar4/homework4/homewok.java
@@ -12,6 +12,9 @@ public class homewok {
     public static void main(String[] args) {
         String exp = "(2^3 * (10 / (5 - 3) ) )^(Sin(Pi ) )";
         System.out.println(CalculatingExpression(exp));
+        StringBuilder sb = new StringBuilder(CalculatingExpression(exp));
+        System.out.println(IsBracketsRight(sb));
+        System.out.println(sb);
     }
 
     static String CalculatingExpression(String exp) {
@@ -23,6 +26,40 @@ public class homewok {
         ArrayDeque<String> dqChar = new ArrayDeque<>();
 
         return sb.toString();
+    }
+
+    static StringBuilder OpeningBrackets(StringBuilder sb, ArrayDeque<String> dqString, ArrayDeque<Double> dqDouble) {
+        int endIndex = sb.indexOf(")");
+        if (endIndex == -1) {
+
+        }
+        return sb;
+    }
+
+    static boolean IsBracketsRight(StringBuilder sb1) {
+        StringBuilder sb = new StringBuilder(sb1);
+        ArrayDeque<Character> tmp = new ArrayDeque<>();
+        int bracketClose = 0;
+        int bracketOpen = sb.indexOf(")", bracketClose);
+        while (bracketClose != -1) {
+            bracketClose = sb.indexOf(")");
+            // System.out.println();
+            if (bracketClose == -1)
+                break;
+            tmp.push(sb.charAt(bracketClose));
+            // System.out.println(bracketClose);
+            sb.deleteCharAt(bracketClose);
+            bracketOpen = sb.lastIndexOf("(", bracketClose);
+            if (bracketOpen == -1)
+                break;
+            if (!tmp.isEmpty()) {
+                // System.out.println(bracketOpen);
+                tmp.pop();
+                sb.deleteCharAt(bracketOpen);
+            }
+        }
+        // System.out.println(sb);
+        return tmp.isEmpty() && sb.indexOf("(") == -1;
     }
 
     static double Calculate(ArrayDeque<String> dqChar, ArrayDeque<Double> dqDouble) {
@@ -40,7 +77,11 @@ public class homewok {
             case "-":
                 res = dqDouble.pop() + dqDouble.pop();
                 break;
-            
+            case "^":
+                res = Math.pow(dqDouble.pop(), dqDouble.pop());
+                break;
+            case "sin":
+                res = Math.sin(dqDouble.pop());
         }
         return res;
     }
